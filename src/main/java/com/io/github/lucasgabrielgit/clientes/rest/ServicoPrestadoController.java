210b3638766c06727769dfb3c41d9530eb030c49
LucasGabrielGit <36990357+LucasGabrielGit@users.noreply.github.com>
2020-07-18 13:58:25 -0300

    updates Serviço Conotroller

--- src/main/java/com/io/github/lucasgabrielgit/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/com/io/github/lucasgabrielgit/clientes/rest/ServicoPrestadoController.java
@@ -21,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/servicos")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoController {
 
     private final ClientesRepository clientesRepository;
